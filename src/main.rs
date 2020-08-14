1cb3b1db34f0b4e8c29665df6271bd89b95e4d25
Jorian <jorian@outlook.com>
2020-08-14 09:26:05 +0200

    initial working scenario

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use komodo_rpc_client::{Client, Chain, KomodoRpcApi};
+use komodo_rpc_client::{Client, Chain, KomodoRpcApi, TransactionId};
 use std::path::Path;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let client = Client::new_assetchain_client(&Chain::Custom(String::from("ILN")))
-        .expect("an ILN client was expected");
+        .expect("a client was expected");
 
-    println!("{}", client.get_info().unwrap().balance);
-    airdrop_fixed_amount(Path::new("./testdata.txt"), 0.2);
+    airdrop_fixed_amount(Path::new("./08-50.txt"), 14.59636933);
 }
 
-fn airdrop_fixed_amount(addies: &Path, amount: f32) -> io::Result<()> {
+fn airdrop_fixed_amount(addies: &Path, amount: f64) -> io::Result<()> {
     let file = File::open(addies)?;
     let reader = BufReader::new(file);
 
@@ -20,23 +21,39 @@ fn airdrop_fixed_amount(addies: &Path, amount: f32) -> io::Result<()> {
         .map(|line| line.unwrap() )
         .collect::<Vec<String>>();
 
-    // println!("{:?}", vec);
+    let client = Client::new_assetchain_client(&Chain::Custom(String::from("ILN")))
+        .expect("a client was expected");
 
-    vec.chunks(777).for_each(|addy| println!("{:?}", addy));
+    for chunk in vec.chunks(777) {
+        let tx = create_and_send(chunk, amount);
+        dbg!(&tx);
+        let mut raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&tx.be_hex_string()).unwrap());
 
-    // read addies
-    // take chunks of 777
-    //prepare sendmany
-    // send sendmany
+        sleep(Duration::from_secs(2));
+
+        while raw_tx.unwrap().confirmations.is_none() {
+            println!("no confirmation, wait 10 sec");
+            sleep(Duration::from_secs(10));
+            raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&tx.be_hex_string()).unwrap());
+            sleep(Duration::from_secs(2));
+        }
+    }
+
     Ok(())
 }
 
-// data is a csv
-fn airdrop_csv(data: &Path) {
+fn create_and_send(chunk: &[String], amount: f64) -> TransactionId {
+    let mut sendmany = komodo_rpc_client::arguments::SendManyAmounts::new();
+    for addy in chunk {
+        let address = komodo_rpc_client::arguments::address::Address::from(addy).unwrap();
+        sendmany.add(&address.to_string(), amount);
+    }
 
-}
+    let client = Client::new_assetchain_client(&Chain::Custom(String::from("ILN")))
+        .expect("a client was expected");
 
-fn create_send_many() {
+    let tx = client.send_many(sendmany, None, None, None);
 
+    tx.unwrap()
 }
 
