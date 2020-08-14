a028dcbf0123b12adc37f5faf1581454c83cacc0
Jorian <jorian@outlook.com>
2020-08-14 09:53:03 +0200

    refactor, write tests

--- src/main.rs
+++ src/main.rs
@@ -6,54 +6,80 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    let client = Client::new_assetchain_client(&Chain::Custom(String::from("ILN")))
+    let client = Client::new_assetchain_client(&Chain::Custom(String::from("MORTY")))
         .expect("a client was expected");
+    let file_name = Path::new("data/08-0.50.txt");
+    let amount = 0.012;
 
-    airdrop_fixed_amount(Path::new("./08-50.txt"), 14.59636933);
-}
+    if let Ok(vec) = read_addresses_file(file_name) {
+        for chunk in vec.chunks(777) {
+            let txid = create_and_send(&client, chunk, amount);
+            let mut raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&txid.be_hex_string()).unwrap());
 
-fn airdrop_fixed_amount(addies: &Path, amount: f64) -> io::Result<()> {
-    let file = File::open(addies)?;
-    let reader = BufReader::new(file);
-
-    let vec = reader
-        .lines()
-        .map(|line| line.unwrap() )
-        .collect::<Vec<String>>();
-
-    let client = Client::new_assetchain_client(&Chain::Custom(String::from("ILN")))
-        .expect("a client was expected");
-
-    for chunk in vec.chunks(777) {
-        let tx = create_and_send(chunk, amount);
-        dbg!(&tx);
-        let mut raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&tx.be_hex_string()).unwrap());
-
-        sleep(Duration::from_secs(2));
-
-        while raw_tx.unwrap().confirmations.is_none() {
-            println!("no confirmation, wait 10 sec");
-            sleep(Duration::from_secs(10));
-            raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&tx.be_hex_string()).unwrap());
             sleep(Duration::from_secs(2));
+
+            while raw_tx.unwrap().confirmations.is_none() {
+                println!("no confirmation, wait 10 sec");
+                sleep(Duration::from_secs(10));
+                raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&txid.be_hex_string()).unwrap());
+                // need to sleep to prevent Hyper IncompleteMessage errors
+                sleep(Duration::from_secs(2));
+            }
         }
     }
-
-    Ok(())
 }
 
-fn create_and_send(chunk: &[String], amount: f64) -> TransactionId {
+fn read_addresses_file(addresses: &Path) -> io::Result<Vec<String>>  {
+    let file = File::open(addresses)?;
+    let reader = BufReader::new(file);
+
+    Ok(reader
+        .lines()
+        .filter_map(Result::ok) // TODO make sure this is a valid address
+        .collect::<Vec<String>>())
+}
+
+fn create_and_send(client: &Client, chunk: &[String], amount: f64) -> TransactionId {
     let mut sendmany = komodo_rpc_client::arguments::SendManyAmounts::new();
     for addy in chunk {
         let address = komodo_rpc_client::arguments::address::Address::from(addy).unwrap();
         sendmany.add(&address.to_string(), amount);
     }
 
-    let client = Client::new_assetchain_client(&Chain::Custom(String::from("ILN")))
-        .expect("a client was expected");
-
     let tx = client.send_many(sendmany, None, None, None);
 
     tx.unwrap()
 }
 
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use crate::read_addresses_file;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn valid_addresses() {
+        let path = Path::new("data/test_valid.txt");
+        let vec = read_addresses_file(path);
+
+        let valid_addresses = vec!{
+            String::from("RAMVr4wrArBMM4j1J5gmCTiE5zvpBR9L3V"),
+            String::from("RHQt6RRAKgzdvZxSPH5CxLNC9zmaN7ARvC"),
+            String::from("RVFh5H8HuaBvAYVngoSoYPijjUvpXjsq1e")
+        };
+
+        assert_eq!(valid_addresses, vec.unwrap());
+    }
+
+    #[test]
+    fn invalid_addresses() {
+        let path = Path::new("data/test_invalid.txt");
+        let vec = read_addresses_file(path);
+
+        assert!(vec.is_err());
+    }
+}
