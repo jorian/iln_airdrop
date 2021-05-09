6f7dd2ab93f02ecc11bbdb5b3c2aa6cedb7b63ce
jorian <10341677+jorian@users.noreply.github.com>
2021-05-09 20:26:48 +0200

    change dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,78 @@
-use komodo_rpc_client::{Client, Chain, KomodoRpcApi, TransactionId};
-use std::path::Path;
-use std::fs::File;
+use komodo_rpc_client::arguments::{address::Address, SendManyAmounts};
+use komodo_rpc_client::{Chain, Client, KomodoRpcApi, TransactionId};
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 use std::io::{self, BufRead, BufReader};
+use std::path::Path;
 use std::thread::sleep;
 use std::time::Duration;
-use komodo_rpc_client::arguments::{
-    address::Address,
-    SendManyAmounts
-};
 
 fn main() {
-    let client = Client::new_assetchain_client(&Chain::Custom(String::from("MORTY")))
+    let client = Client::new_assetchain_client(&Chain::Custom(String::from("ILN")))
         .expect("a client was expected");
-    let file_name = Path::new("data/08-0.50.txt");
-    let amount = 0.012;
+    let file_name = Path::new("data/0805-50.txt");
+    let amount = 2.46983000;
+    let mut outfile = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("data/processed_addresses_1104-50.txt")
+        .unwrap();
 
     if let Ok(vec) = read_addresses_from_file(file_name) {
         for chunk in vec.chunks(777) {
-            let txid = create_and_send(&client, chunk, amount);
-            let mut raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&txid.be_hex_string()).unwrap());
+            let txid = create_and_send(&client, chunk, amount, &mut outfile);
+            println!("{:?}", &txid.to_string());
+            let mut raw_tx = client.get_raw_transaction_verbose(
+                TransactionId::from_hex(&txid.be_hex_string()).unwrap(),
+            );
 
             sleep(Duration::from_secs(2));
 
             while raw_tx.unwrap().confirmations.is_none() {
-                println!("no confirmation, wait 10 sec");
-                sleep(Duration::from_secs(10));
-                raw_tx = client.get_raw_transaction_verbose(TransactionId::from_hex(&txid.be_hex_string()).unwrap());
+                println!("no confirmation, wait 8 sec");
+                sleep(Duration::from_secs(8));
+                raw_tx = client.get_raw_transaction_verbose(
+                    TransactionId::from_hex(&txid.be_hex_string()).unwrap(),
+                );
                 // need to sleep to prevent Hyper IncompleteMessage errors
                 sleep(Duration::from_secs(2));
             }
         }
     } else {
-        println!{"Something went wrong while reading the file"}
+        println! {"Something went wrong while reading the file"}
     }
 }
 
-fn read_addresses_from_file(addresses: &Path) -> io::Result<Vec<Address>>  {
+fn read_addresses_from_file(addresses: &Path) -> io::Result<Vec<Address>> {
     let file = File::open(addresses)?;
     let reader = BufReader::new(file);
 
     let mut vec = vec![];
 
-    reader
-        .lines()
-        .for_each(|line| {
-            let str_add = line.unwrap();
-            match Address::from(&str_add) {
-                Ok(address) => vec.push(address),
-                Err(err) => println!("error parsing address {}: {}", &str_add, err.to_string())
-            }
-        });
+    reader.lines().for_each(|line| {
+        let str_add = line.unwrap();
+        match Address::from(&str_add) {
+            Ok(address) => vec.push(address),
+            Err(err) => println!("error parsing address {}: {}", &str_add, err.to_string()),
+        }
+    });
 
     Ok(vec)
 }
 
-fn create_and_send(client: &Client, chunk: &[Address], amount: f64) -> TransactionId {
+fn create_and_send(
+    client: &Client,
+    chunk: &[Address],
+    amount: f64,
+    outfile: &mut File,
+) -> TransactionId {
     let mut sendmany = SendManyAmounts::new();
     for addy in chunk {
         sendmany.add(&addy.to_string(), amount);
+        match writeln!(outfile, "{}", &addy.to_string()) {
+            Ok(_) => {}
+            Err(e) => println!("{}: {}", &addy.to_string(), e.to_string()),
+        }
     }
 
     let tx = client.send_many(sendmany, None, None, None);
@@ -67,9 +82,9 @@ fn create_and_send(client: &Client, chunk: &[Address], amount: f64) -> Transacti
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
     use crate::read_addresses_from_file;
     use komodo_rpc_client::arguments::address::Address;
+    use std::path::Path;
 
     #[test]
     fn it_works() {
@@ -81,11 +96,11 @@ mod tests {
         let path = Path::new("data/test_valid.txt");
         let vec = read_addresses_from_file(path);
 
-        let valid_addresses = vec!{
+        let valid_addresses = vec![
             Address::from("RAMVr4wrArBMM4j1J5gmCTiE5zvpBR9L3V").unwrap(),
             Address::from("RHQt6RRAKgzdvZxSPH5CxLNC9zmaN7ARvC").unwrap(),
-            Address::from("RVFh5H8HuaBvAYVngoSoYPijjUvpXjsq1e").unwrap()
-        };
+            Address::from("RVFh5H8HuaBvAYVngoSoYPijjUvpXjsq1e").unwrap(),
+        ];
 
         assert_eq!(valid_addresses, vec.unwrap());
     }
